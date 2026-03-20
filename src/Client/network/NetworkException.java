d04f0422d807860327b76dba97559ac0ed580af5
siham <siham.emf35@gmail.com>
2026-03-20 22:56:43 +0000

    implementing NetworkException and adding some changes to the DB

--- src/Client/network/NetworkException.java
+++ src/Client/network/NetworkException.java
@@ -2,9 +2,7 @@ package Client.network;
 
 public class NetworkException extends RuntimeException {
 
-    public NetworkException(String message) {
-        super(message);
-    }
+    public NetworkException(String message) { super(message); }
 
     public NetworkException(String message, Throwable cause) {
         super(message, cause);
